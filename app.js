576705780c99c26e967979d61d27a797cfbe4882
Jaime Toro <zomo505@gmail.com>
2023-07-10 03:53:58 +0200

    didn't commit on friday and lost everything...

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express()
 
@@ -13,7 +14,41 @@ app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+})
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error))
 
 // Our routes go here:
+app.get('/', (request,response) => {
+    response.render('index')
+    
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'))
+app.get('/artist-search', async (request, response) => {
+    
+   try{
+    const searchResult = await spotifyApi.searchArtists(request.query.artist);
+    const results = searchResult.body.artists.items;
+
+    console.log("searchResult:",searchResult);
+    console.log("results:", results);
+    
+    response.render('artist-search-results', { results });
+    
+   }catch (error) {
+    console.log("Error searching artists", error);
+   }
+});
+
+
+
+
+app.listen(3000, () => console.log('My Spotify project running on port 3000'))
+ 
